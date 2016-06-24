337f40acc71c73266fa11e1318da6f5ecb8a172d
Carmen Salvador <carmen.salvador@schibsted.com.mx>
2016-06-24 11:54:00 -0500

    Initial Commit

---
+++ ConektaLib/ConektaLib.h
@@ -0,0 +1,19 @@
+//
+//  ConektaLib.h
+//  ConektaLib
+//
+//  Created by Carmen Salvador Bucio on 6/24/16.
+//  Copyright © 2016 com.segundamanomx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConektaLib.
+FOUNDATION_EXPORT double ConektaLibVersionNumber;
+
+//! Project version string for ConektaLib.
+FOUNDATION_EXPORT const unsigned char ConektaLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConektaLib/PublicHeader.h>
+
+
